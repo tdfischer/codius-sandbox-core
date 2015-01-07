96d4303b3f37511d292ed24a56e544c381368fc3
Torrie Fischer <torrie@ripple.com>
2015-01-07 10:55:05 -0800

    Build crate as a staticlib, and drop bin building for now

--- src/bin/test.rs
+++
@@ -1,9 +0,0 @@
-extern crate "codius-sandbox-core" as sandbox;
-
-fn main() {
-  let mut sbox = sandbox::Sandbox::new();
-  sbox.spawn (&["/bin/true"]);
-  while sbox.running {
-    sbox.tick();
-  }
-}
